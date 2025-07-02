773d3d28f66b4d244a6e6e0f8a0d88ff4a43e70d
JoelRocaCuaternion <developer2@cuaternion.com>
2025-07-02 08:29:41 +0200

    Secure session y contrasenya

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ let app = express();
 
 // Configurar sesiones
 app.use(session({
-    secret: 'cuaternion-forge-secret-key', // Cambia esto por una clave más segura
+    secret: 'VivaEspana!_3', // Cambia esto por una clave más segura
     resave: false,
     saveUninitialized: false,
     cookie: { 
-        secure: false, // Cambiar a true si usas HTTPS
+        secure: true,
         maxAge: 24 * 60 * 60 * 1000 // 24 horas
     }
 }));
